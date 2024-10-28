243e37c5560b7809dbb6b4c300e7edd53846d885
JayasuryaThirumaran <jayasurya.t@talentsprint.com>
2024-10-28 05:57:28 +0000

    sdgdf

--- monitor_transactions.sh
+++ monitor_transactions.sh
@@ -1,5 +1,2 @@
-#!/bin/bash
-
-# Scan the transaction log for withdrawals of ₹50,000 or more
-echo "Suspicious Transactions:"
-grep "Withdraw: [5-9][0-9][0-9][0-9][0-9]\|[1-9][0-9][0-9][0-9][0-9][0-9]+" transaction_log.txt || echo "No suspicious transactions found."
+echo "Suspicious Transactions (Withdrawals of ₹50,000 or more):"
+grep -E "Withdraw: (50000|[5-9][0-9]{4}|[1-9][0-9]{5,})" transaction_log.txt || echo "No suspicious transactions found."
